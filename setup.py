3b44146ee12802a50eeead6babfc5b17c3c9eaa7
Swapneel Pathak <swapiitr@gmail.com>
2022-12-07 20:57:47 +0000

    refactor(setup.py): modify to work with `pyproject.toml`

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-#!/usr/bin/env python
+# This content is required for editable install via pip.
+import setuptools
 
-from distutils.core import setup
-
-setup(name='magnum.nf',
-      version='0.0.1',
-      description='magnum.nf is a micromagnetic GPU code implementing the nodal FD discretization',
-      author='Claas Abert',
-      author_email='claas.abert@univie.ac.at',
-      packages=['magnumnf', 'magnumnf.common', 'magnumnf.field_terms', 'magnumnf.solvers'],
-      #package_data = {},
-      #entry_points={},
-      install_requires=[
-          'numpy',
-          'pyvista'
-          'sympy',
-          'scipy',
-          'torch',
-          'numba'
-      ]
-     )
+setuptools.setup()
