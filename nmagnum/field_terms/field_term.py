553db9e9f559f297dac4db8b7f491d49a562daa9
Claas Abert <claas.abert@univie.ac.at>
2023-09-05 23:56:43 +0200

    first working version with automatic generation

--- nmagnum/field_terms/field_term.py
+++ nmagnum/field_terms/field_term.py
@@ -1,5 +1,74 @@
+from ..generators import pytorch_generator as gen
+from ..common import Function, VectorFunction
+from scipy import constants
+import pathlib
+import importlib
+import sys
+import inspect
+import torch
+
 __all__ = ["FieldTerm"]
 
-
 class FieldTerm(object):
-    pass
+    def __init__(self, *args, **kwargs):
+        this_module = pathlib.Path(importlib.import_module(self.__module__).__file__)
+
+        code_dir = this_module.parent / 'code'
+        code_dir.mkdir(parents = True, exist_ok = True)
+
+        code_module_name = f"{this_module.stem}_code"
+        code_file_path = code_dir / f"{code_module_name}.py"
+
+        if not code_file_path.is_file():
+
+            # generate the code
+            with open(code_file_path, 'w') as code_file:
+                m = gen.Variable('m', 'cg', (3,))
+                e_expr = self.e_expr(m)
+                field_expr = gen.gateaux_derivative(e_expr, m)
+
+                code_file.write("import torch\n")
+                code_file.write(gen.assemble_linear_form(field_expr))
+
+        # import code
+        spec = importlib.util.spec_from_file_location(code_module_name, code_file_path)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[code_module_name] = module
+        spec.loader.exec_module(module)
+        self._code = module
+
+    def h(self, state):
+        if not hasattr(state, '_args'):
+            # set up scratch space for h
+            self._h = VectorFunction(state)
+
+            self._args = [self._h.tensor, state.mesh.dx]
+            args = list(inspect.signature(self._code.assemble_linear_form).parameters.keys())
+            for arg in args[2:]:
+                container = state
+                while '__' in arg:
+                    parent, child = arg.split('__', 1)
+                    container = getattr(container, parent)
+                    arg = child
+                self._args.append(getattr(container, arg).tensor)
+
+            print(self._args)
+
+        self._code.assemble_linear_form(*self._args)
+
+        # compute lumped mass
+        V = state.mesh.cell_volume
+        Ms = state.material.Ms.tensor
+        mass = Function(state).tensor
+        mass[:-1, :-1, :-1] += V / 8.0 * Ms
+        mass[:-1, :-1, 1:] += V / 8.0 * Ms
+        mass[:-1, 1:, :-1] += V / 8.0 * Ms
+        mass[:-1, 1:, 1:] += V / 8.0 * Ms
+        mass[1:, :-1, :-1] += V / 8.0 * Ms
+        mass[1:, :-1, 1:] += V / 8.0 * Ms
+        mass[1:, 1:, :-1] += V / 8.0 * Ms
+        mass[1:, 1:, 1:] += V / 8.0 * Ms
+
+        self._h.tensor.multiply_(-1.0 / (constants.mu_0 * mass.unsqueeze(-1)))
+
+        return self._h
--- tests/unit/exchange_field_test.py
+++ tests/unit/exchange_field_test.py
@@ -15,8 +15,8 @@ def test_h():
     #exchange = ExchangeField()
     #h = exchange.h(state).tensor
 
-    exchange_torch = ExchangeTorchField()
-    h_torch = exchange_torch.h(state).tensor
+    exchange = ExchangeField()
+    h_torch = exchange.h(state).tensor
 
     exchange_torch2 = ExchangeTorchField2()
     h_torch2 = exchange_torch2.h(state).tensor
@@ -24,13 +24,13 @@ def test_h():
     #torch.testing.assert_close(h, h_torch, rtol=1e-8, atol=1.0)
     torch.testing.assert_close(h_torch, h_torch2, rtol=1e-8, atol=1.0)
 
-def test_E():
-    mesh = Mesh((2, 2, 2), (1e-9, 1e-9, 1e-9))
-    state = State(mesh)
-
-    state.m = VectorFunction(state).from_numpy(np.arange(81).reshape(3, 3, 3, 3))
-    state.material.A = CellFunction(state).from_constant(1.2e-11)
-    state.material.Ms = CellFunction(state).from_constant(8e5)
-
-    exchange_torch2 = ExchangeTorchField2()
-    assert exchange_torch2.E(state) == pytest.approx(2.3587200000000006e-16)
+#def test_E():
+#    mesh = Mesh((2, 2, 2), (1e-9, 1e-9, 1e-9))
+#    state = State(mesh)
+#
+#    state.m = VectorFunction(state).from_numpy(np.arange(81).reshape(3, 3, 3, 3))
+#    state.material.A = CellFunction(state).from_constant(1.2e-11)
+#    state.material.Ms = CellFunction(state).from_constant(8e5)
+#
+#    exchange_torch2 = ExchangeTorchField2()
+#    assert exchange_torch2.E(state) == pytest.approx(2.3587200000000006e-16)
