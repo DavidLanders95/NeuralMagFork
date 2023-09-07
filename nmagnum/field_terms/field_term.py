fbe491cc9dc6c0898698b919711350f6bc615c79
Claas Abert <claas.abert@univie.ac.at>
2023-09-07 13:10:50 +0200

    cleanup

--- nmagnum/field_terms/field_term.py
+++ nmagnum/field_terms/field_term.py
@@ -11,7 +11,7 @@ __all__ = ["FieldTerm"]
 
 class FieldTerm(object):
     def __init__(self, *args, **kwargs):
-        # initialize path to code file
+        # initialize code-file cache
         this_module = pathlib.Path(importlib.import_module(self.__module__).__file__)
         code_dir = this_module.parent / 'code'
         code_dir.mkdir(parents = True, exist_ok = True)
@@ -31,12 +31,11 @@ class FieldTerm(object):
 
     def h(self, state):
         if not hasattr(state, '_args'):
-            # set up scratch space for h (TODO do they really need to be persistent?)
+            # set up scratch space for h
             self._h = VectorFunction(state)
-            self._mass = Function(state)
-            self._args = [self._h.tensor, state.mesh.dx, self._mass.tensor]
+            self._args = [self._h.tensor, state.mesh.dx]
             args = list(inspect.signature(self._code.compute_heff).parameters.keys())
-            for arg in args[3:]:
+            for arg in args[2:]:
                 container = state
                 while '__' in arg:
                     parent, child = arg.split('__', 1)
@@ -47,6 +46,10 @@ class FieldTerm(object):
         self._code.compute_heff(*self._args)
 
         return self._h
+
+    @property
+    def hraw(self):
+        return self._code.compute_heff
 
     def generate_code(self):
         with open(self._code_file_path, 'w') as code_file:
@@ -59,9 +62,9 @@ class FieldTerm(object):
             # write header
             code_file.write("import torch\n")
             code_file.write("@torch.compile\n")
-            code_file.write(f"def compute_heff(h, dx, mass, {', '.join(variables)}):\n")
+            code_file.write(f"def compute_heff(h, dx, {', '.join(sorted(variables))}):\n")
 
-            # RHS
+            # linear form
             code_file.write("    h[:] = 0\n")
             for lhs, rhs in cmds.items():
                 code_file.write(f"    {lhs} += {rhs}\n")
@@ -70,7 +73,7 @@ class FieldTerm(object):
             v = gen.Variable('v', 'cg')
             Ms = gen.Variable('material__Ms', 'dg')
             cmds, _ = gen.linear_form_cmds(- constants.mu_0 * Ms * v, 'mass')
-            code_file.write("    mass[:] = 0\n")
+            code_file.write("    mass = torch.zeros(h.shape[:3], dtype = h.dtype, device = h.device)\n")
             for lhs, rhs in cmds.items():
                 code_file.write(f"    {lhs} += {rhs}\n")
             code_file.write("    h /= mass.unsqueeze(-1)\n")
