e442fba1f14860b4ae8fb7342224322e7704ebc8
Claas Abert <claas.abert@univie.ac.at>
2023-10-10 11:09:33 +0200

    allow for user defined parameters in LLG module

--- nmagnum/solvers/llg_solver.py
+++ nmagnum/solvers/llg_solver.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-#from scipy import constants
 from ..common import logging
 from torchdiffeq import odeint_adjoint as odeint
 
@@ -12,16 +11,30 @@ def llg_rhs(h, m, material__alpha):
            - material__alpha * gamma_prime * torch.cross(m, torch.cross(m, h))
 
 class LLGSolver(nn.Module):
-    def __init__(self, state, rtol = 1e-5, atol=1e-5):
+    def __init__(self, state, rtol = 1e-5, atol=1e-5, parameters = []):
         super().__init__()
         self._state = state
         self._rtol = rtol
         self._atop = atol
+
+        self._parameters = {}
+        for param in parameters:
+            # TODO make sure that state.getattr returns a tensor
+            self._parameters[param] = torch.nn.Parameter(state.getattr(param))
+
         self.reset()
 
     def reset(self):
         logging.info_green("[LLGSolver] Initialize RHS function")
-        self._func, self._args = self._state.get_func(llg_rhs, ['t', 'm'])
+
+        internal_args = ['t', 'm']
+        for param in self._parameters.keys():
+            internal_args.append(param)
+
+        self._func, self._args = self._state.get_func(llg_rhs, internal_args)
+
+        for i, param in enumerate(self._parameters.keys()):
+            self._args[2 + i] = self._parameters[param]
 
     def forward(self, t, m):
         return self._func(t * 1e-9, m, *self._args[2:])
