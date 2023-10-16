fix time scaling of LLG and cleanup

demos/inverse_cube/run.py
@@ -10,10 +10,10 @@ mesh = Mesh((2, 2, 2), (5e-9, 5e-9, 5e-9))
 state = State(mesh)
 
 # setup material and m0
-state.material.Ms = CellFunction(state).from_constant(8e5)
-state.material.A = CellFunction(state).from_constant(1.3e-11)
-state.material.Ku = CellFunction(state).from_constant(1e5)
-state.material.Ku_axis = VectorCellFunction(state).from_constant((0, 0, 1))
+state.material.Ms = 8e5
+state.material.A = 1.3e-11
+state.material.Ku = 1e5
+state.material.Ku_axis = [0, 0, 1]
 state.material.alpha = 0.1
 
 state.m = VectorFunction(state).from_constant((0, 0, 1))
@@ -35,25 +35,21 @@ ExternalField(h_ext).register(state, 'external')
 TotalField('exchange', 'aniso', 'external').register(state)
 
 # set up solver, loss function, etc
-llg = LLGSolver(state, scale = 1e-9, parameters = ['phi', 'theta'])
+llg = LLGSolver(state, parameters = ['phi', 'theta'])
 optimizer = torch.optim.Adam(llg.parameters(), lr = 0.05)
 my_loss = torch.nn.L1Loss()
 
 m_target = VectorFunction(state).from_constant((np.sqrt(0.5), 0, np.sqrt(0.5))).tensor
 
-with open('log.dat', 'w') as f:
-    for epoch in range(100):
-        print(f"epoch: {epoch}")
+logger = ScalarLogger('log.dat', ['epoch', 'phi', 'theta', 'loss'])
+for epoch in range(100):
+    print(f"epoch: {epoch}")
+    state.epoch = epoch
 
-        #ts = torch.arange(0., 0.5, 1e-3, dtype = state.dtype, device = state.device)
-        #m_pred = odeint(llg, state.m.tensor, ts)
+    optimizer.zero_grad()
+    m_pred = llg.solve(state.tensor([0., 0.05e-9]))
+    state.loss = my_loss(m_pred[-1], m_target)
+    state.loss.backward()
+    optimizer.step()
 
-        optimizer.zero_grad()
-        m_pred = odeint(llg, state.m.tensor, state.tensor([0., 0.05]))
-        loss = my_loss(m_pred[-1], m_target)
-        loss.backward()
-        optimizer.step()
-
-        values = tuple([epoch] + [x.clone().detach().cpu().item() for x in (state.phi, state.theta, loss)])
-        f.write("%d %g %g %g\n" % values)
-        f.flush()
+    logger.log(state)

demos/sp4/run.py
@@ -6,8 +6,8 @@ mesh = Mesh((100, 25, 1), (5e-9, 5e-9, 3e-9))
 state = State(mesh)
 
 # setup material and m0
-state.material.Ms = CellFunction(state).from_constant(8e5)
-state.material.A = CellFunction(state).from_constant(1.3e-11)
+state.material.Ms = 8e5
+state.material.A = 1.3e-11
 state.material.alpha = 1.
 
 state.m = VectorFunction(state).from_constant((np.sqrt(0.5), np.sqrt(0.5), 0))
@@ -20,8 +20,8 @@ ExternalField(h_ext).register(state, 'external')
 TotalField('exchange', 'demag', 'external').register(state)
 
 # relax to s-state
-llg = LLGSolver(state, scale = 1e-9)
-llg.step(1.)
+llg = LLGSolver(state)
+llg.step(1e-9)
 
 # set external field and perform switch
 h_ext.tensor[..., :] = state.tensor([-19576., 3421., 0.])
@@ -30,4 +30,4 @@ state.material.alpha = 0.02
 logger = Logger('data', ['t', 'm'], ['m'])
 for i in range(100):
     logger.log(state)
-    llg.step(1e-2)
+    llg.step(1e-11)

nmagnum/common/state.py
@@ -75,13 +75,11 @@ class State(object):
             name = child
         return getattr(container, name)
 
-    def tensor(self, value):
+    def tensor(self, value, requires_grad = False):
         if isinstance(value, torch.Tensor):
             # TODO check dtype and device?
             return value
-        #if isinstance(value, np.ndarray):
-        #    return torch.from_numpy(value).to(device = self.device, dtype = self.dtype)
-        return torch.tensor(value, device = self.device, dtype = self.dtype, requires_grad = False)
+        return torch.tensor(value, device = self.device, dtype = self.dtype, requires_grad = requires_grad)
 
     def __getattr__(self, name):
         if callable(self._attr_values[name]):
@@ -115,6 +113,12 @@ class State(object):
             value = value[0]
         else:
             self._attr_types.pop(name, None)
+
+        if isinstance(value, list):
+            try:
+                value = self.tensor(value)
+            except ValueError:
+                pass
 
         self._attr_values[name] = value
         self._attr_funcs.clear()
@@ -152,6 +156,7 @@ class State(object):
                 code += f"    {func_name} = __{func_name}({', '.join(list(inspect.signature(func).parameters.keys()))})\n"
             func_pointers[f"__{name}"] = f
             code += f"    return __{name}({', '.join(list(inspect.signature(f).parameters.keys()))})\n"
+            print(code)
             compiled_code = compile(code, "<string>", "exec")
             func = types.FunctionType(compiled_code.co_consts[0], func_pointers, name)
         else:

nmagnum/solvers/llg_solver.py
@@ -11,10 +11,10 @@ def llg_rhs(h, m, material__alpha):
            - material__alpha * gamma_prime * torch.linalg.cross(m, torch.linalg.cross(m, h))
 
 class LLGSolver(nn.Module):
-    def __init__(self, state, scale = 1., rtol = 1e-5, atol=1e-5, parameters = []):
+    def __init__(self, state, scale_t = 1e-9, rtol = 1e-5, atol=1e-5, parameters = []):
         super().__init__()
         self._state = state
-        self._scale = scale
+        self._scale_t = scale_t
         self._rtol = rtol
         self._atop = atol
 
@@ -38,12 +38,15 @@ class LLGSolver(nn.Module):
             self._args[2 + i] = self._parameters[param]
 
     def forward(self, t, m):
-        return self._scale * self._func(t * self._scale, m, *self._args[2:])
+        return self._scale_t * self._func(t * self._scale_t, m, *self._args[2:])
 
     def step(self, dt):
-        logging.info_blue(f"[LLGSolver] Step: dt = {dt * self._scale:g}s, t = {self._state.t * self._scale:g}s")
-        t = self._state.tensor([self._state.t, (self._state.t + dt)])
+        logging.info_blue(f"[LLGSolver] Step: dt = {dt:g}s, t = {self._state.t:g}s")
+        t = self._state.tensor([self._state.t / self._scale_t, (self._state.t + dt) / self._scale_t])
         m_next = odeint(self, self._state.m.tensor, t, # TODO need to clone m?
                 method = 'dopri5', rtol = 1e-5, atol = 1e-5)
-        self._state.t.fill_(t[-1])
+        self._state.t.fill_(t[-1] * self._scale_t)
         self._state.m.tensor[:] = m_next[-1]
+
+    def solve(self, t):
+        return odeint(self, self._state.m.tensor, t / self._scale_t)