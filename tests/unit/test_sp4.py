3e1fe426bcbce4386274412916e526134074cc3e
Sam Holt <samjrholt@gmail.com>
2024-01-11 11:28:11 +0100

    add plotting

--- tests/unit/test_sp4.py
+++ tests/unit/test_sp4.py
@@ -1,3 +1,7 @@
+import datetime
+import os
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pytest
@@ -5,19 +9,27 @@ import pytest
 from neuralmag import *
 
 
+def create_unique_directory():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    directory = f"tests/test_artifacts/{timestamp}"
+    os.makedirs(directory, exist_ok=True)
+    return directory
+
+
 # How the s_state_init.vti file was produced
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def s_state():
-    mesh = Mesh([100, 25, 1], [5e-09, 5e-09, 1e-09], [0.0, 0.0, 0.0])
+    mesh = Mesh([100, 25, 1], [5e-09, 5e-09, 3e-09], [0.0, 0.0, 0.0])
     state = State(mesh)
 
     state.material.Ms = 800000.0
     state.material.A = 1.3e-11
-    state.material.alpha = 0.5
+    state.material.alpha = 1
     h_ext = VectorFunction(state).from_constant((0, 0, 0))
 
     # initial s-state
-    state.m = state.read_vti("tests/unit/data/s_state_init.vti", "m")
+    state.m = VectorFunction(state).from_constant((1, 0.25, 0.1))
+    # state.read_vti("tests/unit/data/s_state_init.vti", "m")
 
     ExchangeField().register(state, "exchange")
     DemagField().register(state, "demag")
@@ -25,36 +37,25 @@ def s_state():
     TotalField("exchange", "demag", "external").register(state)
     # Make sure s-state is relaxed
     llg = LLGSolver(state)
-    llg.step(1e-9)
+    llg.step(10e-10)
+
+    state.t = 0.0
 
     return state
 
 
-def test_sp4_field_switch_1():
-    mesh = Mesh([100, 25, 1], [5e-09, 5e-09, 3e-09], [0.0, 0.0, 0.0])
-    state = State(mesh)
-
-    state.material.Ms = 800000.0
-    state.material.A = 1.3e-11
-    state.material.alpha = 0.02
-    h_ext = VectorFunction(state).from_constant(
+def test_sp4_field_switch_1(s_state):
+    s_state.material.alpha = 0.02
+    s_state.h_external = VectorFunction(s_state).from_constant(
         (-19576.05800030313, 3421.8312764757497, 0.0)
     )
 
-    # initial s-state
-    state.m = state.read_vti("tests/unit/data/s_state_init.vti", "m")
-
-    ExchangeField().register(state, "exchange")
-    DemagField().register(state, "demag")
-    ExternalField(h_ext).register(state, "external")
-    TotalField("exchange", "demag", "external").register(state)
-
-    llg = LLGSolver(state)
+    llg = LLGSolver(s_state)
 
     logger = Logger("data", ["t", "m"], ["m"])
     for _ in range(30):
         llg.step(1.0e-11)
-        logger.log(state)
+        logger.log(s_state)
 
     data_oommf = pd.read_csv(
         "tests/unit/data/oommf_sp4_fieldswitch_1.csv",
@@ -70,34 +71,33 @@ def test_sp4_field_switch_1():
 
     difference = data["m_y"] - np.interp(data["t"], data_oommf["t"], data_oommf["m_y"])
 
-    assert all(abs(difference) < 0.04)
+    try:
+        assert all(abs(difference) < 0.04)
+    except AssertionError:
+        directory = create_unique_directory()
+        plot_path = os.path.join(directory, "sp4_failure_plot_field_switch_1.png")
+        plt.plot(data["t"], data["m_y"], label="neural_mag")
+        plt.plot(data_oommf["t"], data_oommf["m_y"], label="oommf")
+        plt.legend()
+        plt.xlim([0, 3e-10])
+        plt.xlabel("t (s)")
+        plt.ylabel("my (normalised)")
+        plt.savefig(plot_path)
+        raise
 
 
-def test_sp4_field_switch_2():
-    mesh = Mesh([100, 25, 1], [5e-09, 5e-09, 3e-09], [0.0, 0.0, 0.0])
-    state = State(mesh)
-
-    state.material.Ms = 800000.0
-    state.material.A = 1.3e-11
-    state.material.alpha = 0.02
-    h_ext = VectorFunction(state).from_constant(
+def test_sp4_field_switch_2(s_state):
+    s_state.material.alpha = 0.02
+    s_state.h_external = VectorFunction(s_state).from_constant(
         (-28250.00239881142, -5013.380707394703, 0.0)
     )
 
-    # initial s-state
-    state.m = state.read_vti("tests/unit/data/s_state_init.vti", "m")
-
-    ExchangeField().register(state, "exchange")
-    DemagField().register(state, "demag")
-    ExternalField(h_ext).register(state, "external")
-    TotalField("exchange", "demag", "external").register(state)
-
-    llg = LLGSolver(state)
+    llg = LLGSolver(s_state)
 
     logger = Logger("data", ["t", "m"], ["m"])
     for _ in range(30):
         llg.step(1.0e-11)
-        logger.log(state)
+        logger.log(s_state)
 
     data_oommf = pd.read_csv(
         "tests/unit/data/oommf_sp4_fieldswitch_2.csv",
@@ -113,4 +113,16 @@ def test_sp4_field_switch_2():
 
     difference = data["m_y"] - np.interp(data["t"], data_oommf["t"], data_oommf["m_y"])
 
-    assert all(abs(difference) < 0.04)
+    try:
+        assert all(abs(difference) < 0.04)
+    except AssertionError:
+        directory = create_unique_directory()
+        plot_path = os.path.join(directory, "sp4_failure_plot_field_switch_2.png")
+        plt.plot(data["t"], data["m_y"], label="neural_mag")
+        plt.plot(data_oommf["t"], data_oommf["m_y"], label="oommf")
+        plt.legend()
+        plt.xlim([0, 3e-10])
+        plt.xlabel("t (s)")
+        plt.ylabel("my (normalised)")
+        plt.savefig(plot_path)
+        raise
