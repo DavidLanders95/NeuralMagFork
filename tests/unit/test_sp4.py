6f1bd3b45f8e5575facba5c45113dce9566d01c4
Sam Holt <samjrholt@gmail.com>
2024-01-11 10:19:37 +0100

    new s_state

--- tests/unit/test_sp4.py
+++ tests/unit/test_sp4.py
@@ -3,9 +3,10 @@ import pytest
 import pandas as pd
 import numpy as np
 
+# How the s_state_init.vti file was produced
 @pytest.fixture(scope="function")
 def s_state():
-    mesh = Mesh([100, 25, 3], [5e-09, 5e-09, 1e-09], [0.0, 0.0, 0.0])
+    mesh = Mesh([100, 25, 1], [5e-09, 5e-09, 1e-09], [0.0, 0.0, 0.0])
     state = State(mesh)
 
     state.material.Ms = 800000.0
@@ -26,17 +27,29 @@ def s_state():
     
     return state
 
-def test_sp4_field_switch_1(s_state):
-    # set external field and perform switch
-    s_state.material.alpha = 0.02
-    s_state.h_external = VectorFunction(s_state).from_constant((-19576.05800030313, 3421.8312764757497, 0.0))
+def test_sp4_field_switch_1():
+    mesh = Mesh([100, 25, 1], [5e-09, 5e-09, 3e-09], [0.0, 0.0, 0.0])
+    state = State(mesh)
 
-    llg = LLGSolver(s_state)
+    state.material.Ms = 800000.0
+    state.material.A = 1.3e-11
+    state.material.alpha = 0.02
+    h_ext = VectorFunction(state).from_constant((-19576.05800030313, 3421.8312764757497, 0.0))
+
+    # initial s-state
+    state.m = state.read_vti("tests/unit/data/s_state_init.vti", 'm')
+
+    ExchangeField().register(state, "exchange")
+    DemagField().register(state, "demag")
+    ExternalField(h_ext).register(state, "external")
+    TotalField("exchange", "demag", "external").register(state)
+
+    llg = LLGSolver(state)
 
     logger = Logger('data', ['t', 'm'], ['m'])
     for _ in range(30):
         llg.step(1.0e-11)
-        logger.log(s_state)
+        logger.log(state)
         
     data_oommf = pd.read_csv(
         "tests/unit/data/oommf_sp4_fieldswitch_1.csv",
@@ -55,17 +68,29 @@ def test_sp4_field_switch_1(s_state):
     assert all(abs(difference) < 0.04)
     
     
-def test_sp4_field_switch_2(s_state):
-    # set external field and perform switch
-    s_state.material.alpha = 0.02
-    s_state.h_external = VectorFunction(s_state).from_constant((-28250.00239881142, -5013.380707394703, 0.0))
+def test_sp4_field_switch_2():
+    mesh = Mesh([100, 25, 1], [5e-09, 5e-09, 3e-09], [0.0, 0.0, 0.0])
+    state = State(mesh)
 
-    llg = LLGSolver(s_state)
+    state.material.Ms = 800000.0
+    state.material.A = 1.3e-11
+    state.material.alpha = 0.02
+    h_ext = VectorFunction(state).from_constant((-28250.00239881142, -5013.380707394703, 0.0))
+    
+    # initial s-state
+    state.m = state.read_vti("tests/unit/data/s_state_init.vti", 'm')
+    
+    ExchangeField().register(state, "exchange")
+    DemagField().register(state, "demag")
+    ExternalField(h_ext).register(state, "external")
+    TotalField("exchange", "demag", "external").register(state)
+
+    llg = LLGSolver(state)
 
     logger = Logger('data', ['t', 'm'], ['m'])
     for _ in range(30):
         llg.step(1.0e-11)
-        logger.log(s_state)
+        logger.log(state)
         
     data_oommf = pd.read_csv(
         "tests/unit/data/oommf_sp4_fieldswitch_2.csv",
