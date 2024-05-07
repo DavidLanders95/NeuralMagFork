643a388f27d8c56073e35db8d87b5747346fa6ff
Sam Holt <samjrholt@gmail.com>
2024-05-08 00:00:44 +0200

    change to  event based mininisation

--- neuralmag/solvers/relax.py
+++ neuralmag/solvers/relax.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torchdiffeq import odeint_adjoint as odeint
+from torchdiffeq import odeint_event
 
 from ..common import Function, logging
 from .llg_solver import LLGSolver
@@ -10,9 +11,11 @@ __all__ = ["RelaxSolver"]
 
 def relax_rhs(h, m, material__alpha):
     gamma_prime = 221276.14725379366 / (1.0 + material__alpha**2)
-    return (
-        -material__alpha * gamma_prime * torch.linalg.cross(m, torch.linalg.cross(m, h))
-    )
+    return -material__alpha * gamma_prime * torque(h, m)
+
+
+def torque(h, m):
+    return torch.linalg.cross(m, torch.linalg.cross(m, h))
 
 
 class RelaxSolver(LLGSolver):
@@ -44,23 +47,33 @@ class RelaxSolver(LLGSolver):
             internal_args.append(param)
 
         self._func, self._args = self._state.get_func(relax_rhs, internal_args)
+        self._func_torque, self._args_torque = self._state.get_func(
+            torque, internal_args
+        )
 
         for i, param in enumerate(self._parameters.keys()):
             self._args[2 + i] = self._parameters[param]
+            self._args_torque[2 + i] = self._parameters[param]
 
-    def minimize(self, tol, timestep=1e-10):
+    def minimize(self, tol):
+        self.tol = tol
         t0 = self._state.t
-        E_current = self._state.E
-        logging.info_blue(f"[RelaxSolver] Initial Energy E = {E_current:g} J")
-        dE = 2 * tol  # double tolarance to make sure first step occurs
-
-        while dE > tol:
-            E_prev = E_current
-            self.step(timestep, logging=False)
-            E_current = self._state.E
-            dE = E_prev - E_current
-            logging.info_blue(
-                f"[RelaxSolver]  dE = {dE:g} J, tol = {tol:g} J, E = {E_current:g} J"
-            )
-
+        logging.info_blue(f"[RelaxSolver] Initial Energy E = {self._state.E:g} J")
+        _, m_next = odeint_event(
+            self,
+            self._state.m.tensor,
+            self._state.t,
+            event_fn=self._event_fn,
+            odeint_interface=odeint,
+            **self._solver_options,
+        )
+        self._state.m.tensor[:] = m_next[-1]
+        logging.info_blue(f"[RelaxSolver] Final Energy E = {self._state.E:g} J")
         self._state.t = t0
+
+    def _event_fn(self, t, m):
+        torque = self._func_torque(t * self._scale_t, m, *self._args_torque[2:])
+        if torque.abs().max() > self.tol:
+            return torch.ones_like(t)
+        else:
+            return torch.zeros_like(t)
