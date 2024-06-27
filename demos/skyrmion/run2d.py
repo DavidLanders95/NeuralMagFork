4db6b4f67c751f1a2c3bdeaf48fdea71f0aff2d5
Claas Abert <claas.abert@univie.ac.at>
2024-06-27 22:47:08 +0200

    import neural as nm in all demos

--- demos/skyrmion/run2d.py
+++ demos/skyrmion/run2d.py
@@ -1,11 +1,11 @@
 import torch
 from scipy import constants
 
-from neuralmag import *
+import neuralmag as nm
 
 # setup state
-mesh = Mesh((50, 50), (2e-9, 2e-9, 0.6e-9), (-50e-9, -50e-9, 0))
-state = State(mesh)
+mesh = nm.Mesh((50, 50), (2e-9, 2e-9, 0.6e-9), (-50e-9, -50e-9, 0))
+state = nm.State(mesh)
 
 state.material.Ms = 1.0 / constants.mu_0
 state.material.A = 1.6e-11
@@ -16,21 +16,21 @@ state.material.Ku_axis = [0, 0, 1]
 state.material.alpha = 0.1
 
 # set circular geometry
-state.rho = CellFunction(state).fill(state.eps)
+state.rho = nm.CellFunction(state).fill(state.eps)
 x, y = state.coordinates()
 state.rho.tensor[x**2.0 + y**2.0 < 50e-9**2.0] = 1.0
 
 # initial magnetization
-state.m = VectorFunction(state).fill((0, 0, 1))
+state.m = nm.VectorFunction(state).fill((0, 0, 1))
 
 ## register effective field
-ExchangeField().register(state, "exchange")
-DemagField().register(state, "demag")
-InterfaceDMIField().register(state, "dmi")
-UniaxialAnisotropyField().register(state, "aniso")
-TotalField("exchange", "demag", "dmi", "aniso").register(state)
+nm.ExchangeField().register(state, "exchange")
+nm.DemagField().register(state, "demag")
+nm.InterfaceDMIField().register(state, "dmi")
+nm.UniaxialAnisotropyField().register(state, "aniso")
+nm.TotalField("exchange", "demag", "dmi", "aniso").register(state)
 
 # relax to skyrmion
-llg = LLGSolver(state)
+llg = nm.LLGSolver(state)
 llg.step(1e-9)
 state.write_vti(["m", "rho"], "skyrmion.vti")
