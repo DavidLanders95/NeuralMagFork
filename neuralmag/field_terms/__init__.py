d8ede0e63e64814d8e2ce2242cbaf0b9e8c4afa4
Sam Holt <samjrholt@gmail.com>
2025-09-16 17:55:26 +0200

    refactor: remove dmi_field module and update imports in __init__.py

--- neuralmag/field_terms/__init__.py
+++ neuralmag/field_terms/__init__.py
@@ -3,7 +3,6 @@
 from .bulk_dmi_field import *
 from .cubic_anisotropy_field import *
 from .demag_field import *
-from .dmi_field import *
 from .exchange_field import *
 from .external_field import *
 from .field_term import *
@@ -24,6 +23,5 @@ __all__ = (
     + total_field.__all__
     + uniaxial_anisotropy_field.__all__
     + cubic_anisotropy_field.__all__
-    + dmi_field.__all__
     + li_field.__all__
 )
